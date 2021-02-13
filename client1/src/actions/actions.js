1f418950bd84da5a1c75e28df306d53ca24c0222
Pawan <pawanjangle1000@gmail.com>
2021-02-13 14:08:11 +0530

    actions.js deleted

--- client1/src/actions/actions.js
+++
@@ -1,6 +0,0 @@
-// import axios from "axios";
-// export const signupUser = (data) => dispatch => {
-// axios.post("/user/signup", data).then(res=>
-//     console.log(data);
-//     console.log(res));
-// }
